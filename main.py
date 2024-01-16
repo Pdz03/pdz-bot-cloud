57e66b1ca206a9b31f10d44e4b5811ea07a8a01e
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-01-16 17:11:05 +0700

    Percobaan

--- main.py
+++ main.py
@@ -34,29 +34,31 @@ def auth_telegram_token(x_telegram_bot_api_secret_token: str = Header(None)) -> 
 
 
 @app.post("/webhook/")
-async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
-    chat_id = update.message["chat"]["id"]
-    text = update.message["text"]
-    # print("Received message:", update.message)
-
-    if text == "/start":
-        await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
-    elif text == "Haloo":
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Haloo juga!")
-    else:
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Maaf, bot ini hanya digunakan untuk upload file")
-
-    return {"ok": True}
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(
+        message, "Selamat datang di Pdz Cloud, silakan upload filemu di sini!\n\nTekan /help untuk informasi lebih lanjut")
 
 
-# @bot.message_handler(commands=['start'])
-# def send_welcome(message):
-#     bot.reply_to(message, "Selamat datang di Pdz Cloud, silakan upload filemu di sini!\n\nTekan /help untuk informasi lebih lanjut")
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Selamat datang di Pdz Cloud, anda dapat mengupload file apa saja di sini.\n\nJadikan bot ini sebagai media penyimpanan pribadimu.\n\nGunakan bot ini dengan bijak.\n\nJika terjadi kendala selama penggunaan bot, hubungi akun ini @Pdz03\n\nTerimakasih")
 
-# @bot.message_handler(commands=['help'])
-# def send_help(message):
-#     bot.reply_to(message, "Selamat datang di Pdz Cloud, anda dapat mengupload file apa saja di sini.\n\nJadikan bot ini sebagai media penyimpanan pribadimu.\n\nGunakan bot ini dengan bijak.\n\nJika terjadi kendala selama penggunaan bot, hubungi akun ini @Pdz03\n\nTerimakasih")
 
-# @bot.message_handler(content_types=['document', 'audio', 'photo', 'video'])
-# def send_respon(message):
-#     bot.reply_to(message, "Yeayy, file berhasil terupload!")
+@bot.message_handler(content_types=['document', 'audio', 'photo', 'video'])
+def send_respon(message):
+    bot.reply_to(message, "Yeayy, file berhasil terupload!")
+
+# async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_telegram_token)):
+#     chat_id = update.message["chat"]["id"]
+#     text = update.message["text"]
+#     # print("Received message:", update.message)
+
+#     if text == "/start":
+#         await bot.send_message(chat_id=chat_id, text="Welcome to Cyclic Starter Python Telegram Bot!")
+#     elif text == "Haloo":
+#         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Haloo juga!")
+#     else:
+#         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Maaf, bot ini hanya digunakan untuk upload file")
+
+#     return {"ok": True}
