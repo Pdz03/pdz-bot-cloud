936fbae25392f9b6743432ffa024ce9b31c2146d
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-01-16 20:44:07 +0700

    test photo

--- main.py
+++ main.py
@@ -51,8 +51,8 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
         await bot.send_message(chat_id=chat_id, text="Selamat datang di Pdz Cloud, anda dapat mengupload file apa saja di sini.\n\nJadikan bot ini sebagai media penyimpanan pribadimu.\n\nGunakan bot ini dengan bijak.\n\nJika terjadi kendala selama penggunaan bot, hubungi akun ini @Pdz03\n\nTerimakasih")
     elif text == "/help":
         await bot.send_message(chat_id=chat_id, text="Selamat Dataaaaaaaang")
-    # elif document:
-    #     await bot.send_message(chat_id=chat_id, text="Yeayy, file berhasil terupload")
+    elif update.message["photo"]:
+        await bot.send_message(chat_id=chat_id, text="Yeayy, file berhasil terupload")
     else:
         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Yo!")
 
