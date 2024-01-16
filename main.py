f3997634a16ca70fce0e9ccfda9999703ca09553
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-01-16 16:56:19 +0700

    fix no enter

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ async def handle_webhook(update: TelegramUpdate, token: str = Depends(auth_teleg
     # print("Received message:", update.message)
 
     if text == "/start":
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Selamat datang di Pdz Cloud, silakan upload filemu di sini!\n\nTekan /help untuk informasi lebih lanjut")
+        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Selamat datang di Pdz Cloud, silakan upload filemu di sini!Tekan /help untuk informasi lebih lanjut")
     elif text == "/help":
-        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Selamat datang di Pdz Cloud, anda dapat mengupload file apa saja di sini.\n\nJadikan bot ini sebagai media penyimpanan pribadimu.\n\nGunakan bot ini dengan bijak.\n\nJika terjadi kendala selama penggunaan bot, hubungi akun ini @Pdz03\n\nTerimakasih")
+        await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Selamat datang di Pdz Cloud, anda dapat mengupload file apa saja di sini.Jadikan bot ini sebagai media penyimpanan pribadimu.Gunakan bot ini dengan bijak.Jika terjadi kendala selama penggunaan bot, hubungi akun ini @Pdz03Terimakasih")
     elif text == "Haloo":
         await bot.send_message(chat_id=chat_id, reply_to_message_id=update.message["message_id"], text="Haloo juga!")
     else:
